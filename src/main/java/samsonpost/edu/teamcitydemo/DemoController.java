f98b4f4ab8692cac6c68dcdafbdbabe7536db055
eobukhova <eobukhova@samsonpost.ru>
2024-10-24 16:22:52 +0300

    init commit

---
+++ src/main/java/samsonpost/edu/teamcitydemo/DemoController.java
@@ -0,0 +1,16 @@
+package samsonpost.edu.teamcitydemo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+@RequiredArgsConstructor
+public class DemoController {
+    @GetMapping("/helloworld")
+    public String returnHelloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/samsonpost/edu/teamcitydemo/TeamCityDemoApplication.java
@@ -0,0 +1,12 @@
+package samsonpost.edu.teamcitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCityDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamCityDemoApplication.class, args);
+    }
+
+}
